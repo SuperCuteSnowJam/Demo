c10615a616ff2b5e784176030e34e6c0bad188a3
Guoqiang.wang <earlydata468>
2020-06-04 16:06:57 +0800

    spark demo

--- src/main/java/com/guoqiang/demo/javademo/SparkDemo.java
+++ src/main/java/com/guoqiang/demo/javademo/SparkDemo.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkDemo {
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf();
         conf.set("spark.debug.maxToStringFields", "100");
